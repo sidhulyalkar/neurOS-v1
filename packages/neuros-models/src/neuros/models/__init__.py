aa4ca9a482eed171fe6e0f157415558d1f0c1827
Sidharth Hulyalkar <sid.soccer.21@gmail.com>
2026-08-24 17:27:01 -0700

    models: export Braindecode adapter

--- packages/neuros-models/src/neuros/models/__init__.py
+++ packages/neuros-models/src/neuros/models/__init__.py
@@ -7,28 +7,30 @@ from neuros.models.analysis import (
     MechanisticallyInspectable,
     validate_manifest_paths,
 )
-from neuros.models.base_model import BaseModel
-from neuros.models.catalog import DecoderCard, get_decoder_card, list_decoder_cards
-from neuros.models.simple_classifier import SimpleClassifier
-from neuros.models.eegnet_model import EEGNetModel
-from neuros.models.cnn_model import CNNModel
-from neuros.models.random_forest_model import RandomForestModel
-from neuros.models.svm_model import SVMModel
-from neuros.models.knn_model import KNNModel
-from neuros.models.gbdt_model import GBDTModel
-from neuros.models.transformer_model import TemporalTransformerModel, TransformerModel
-from neuros.models.eeg_conformer_model import EEGConformerModel
-from neuros.models.dino_v3_model import DinoV3Model
-from neuros.models.lstm_model import LSTMModel
-from neuros.models.composite_model import CompositeModel
 from neuros.models.attention_fusion_model import AttentionFusionModel
+from neuros.models.base_model import BaseModel
+from neuros.models.braindecode_adapter import BraindecodeDecoder
+from neuros.models.catalog import DecoderCard, get_decoder_card, list_decoder_cards
+from neuros.models.cnn_model import CNNModel
+from neuros.models.composite_model import CompositeModel
+from neuros.models.dino_v3_model import DinoV3Model
+from neuros.models.eeg_conformer_model import EEGConformerModel
+from neuros.models.eegnet_model import EEGNetModel
+from neuros.models.gbdt_model import GBDTModel
+from neuros.models.knn_model import KNNModel
+from neuros.models.lstm_model import LSTMModel
 from neuros.models.model_registry import ModelMetadata, ModelRegistry
+from neuros.models.random_forest_model import RandomForestModel
+from neuros.models.simple_classifier import SimpleClassifier
+from neuros.models.svm_model import SVMModel
+from neuros.models.transformer_model import TemporalTransformerModel, TransformerModel
 
 __all__ = [
     "AnalysisCapability",
     "AnalysisSurface",
     "AttentionFusionModel",
     "BaseModel",
+    "BraindecodeDecoder",
     "CNNModel",
     "CompositeModel",
     "DecoderCard",
