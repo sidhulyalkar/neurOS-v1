95a33de6b4a737c0e0ee1dd86f8dedb8980b5575
Sidharth Hulyalkar <sid.soccer.21@gmail.com>
2026-08-31 15:31:25 -0700

    feat: export ExecutionConfig authority

--- packages/neuros-core/src/neuros/config/__init__.py
+++ packages/neuros-core/src/neuros/config/__init__.py
@@ -1,9 +1,17 @@
 """Configuration contracts and compilation for neurOS."""
 
 from .build import ResolvedPipeline, ResolvedStream, resolve_config
-from .schema import PipelineConfig, PluginConfig, RuntimeConfig, StreamConfig, load_config
+from .schema import (
+    ExecutionConfig,
+    PipelineConfig,
+    PluginConfig,
+    RuntimeConfig,
+    StreamConfig,
+    load_config,
+)
 
 __all__ = [
+    "ExecutionConfig",
     "PipelineConfig",
     "PluginConfig",
     "ResolvedPipeline",
