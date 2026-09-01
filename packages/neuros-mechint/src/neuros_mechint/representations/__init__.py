e35e7691b258753aa85cd1e1b38abb57985b43d9
Sidharth Hulyalkar <sid.soccer.21@gmail.com>
2026-08-31 17:23:15 -0700

    staging: expose representation benchmark package

---
+++ packages/neuros-mechint/src/neuros_mechint/representations/__init__.py
@@ -0,0 +1,55 @@
+"""Boundary-preserving neural representation comparison tools."""
+
+from .autoencoder import AutoencoderRepresentation
+from .benchmark import RepresentationBenchmark
+from .contracts import (
+    FitRegime,
+    MethodOutcome,
+    MethodStatus,
+    RepresentationBenchmarkResult,
+    RepresentationEmbedding,
+    RepresentationError,
+    RepresentationMethod,
+    RepresentationUnavailableError,
+    SequenceBatch,
+)
+from .external import PrecomputedTemporalSSLRepresentation
+from .metrics import (
+    aggregate_geometry_metrics,
+    local_neighborhood_preservation,
+    pairwise_distance_rank_preservation,
+    temporal_continuity_ratio,
+)
+from .pca import PCARepresentation
+from .tphate import (
+    TPHATEEmbeddingError,
+    TPHATERepresentation,
+    TPHATEUnavailableError,
+    UPSTREAM_LICENSE_NOTICE,
+    UPSTREAM_REPOSITORY,
+)
+
+__all__ = [
+    "AutoencoderRepresentation",
+    "FitRegime",
+    "MethodOutcome",
+    "MethodStatus",
+    "PCARepresentation",
+    "PrecomputedTemporalSSLRepresentation",
+    "RepresentationBenchmark",
+    "RepresentationBenchmarkResult",
+    "RepresentationEmbedding",
+    "RepresentationError",
+    "RepresentationMethod",
+    "RepresentationUnavailableError",
+    "SequenceBatch",
+    "TPHATEEmbeddingError",
+    "TPHATERepresentation",
+    "TPHATEUnavailableError",
+    "UPSTREAM_LICENSE_NOTICE",
+    "UPSTREAM_REPOSITORY",
+    "aggregate_geometry_metrics",
+    "local_neighborhood_preservation",
+    "pairwise_distance_rank_preservation",
+    "temporal_continuity_ratio",
+]
